c38dda4dc63e480f1b4a5b11323dc55bfc9a70bb
Monjur Ahmed Akash <monjur3069@gmail.com>
2022-09-16 17:47:49 +0600

    User_Current_Locaion

--- googlemap/lib/main.dart
+++ googlemap/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:googlemap/homepage.dart';
 
 import 'convert_latlang_flutter_geocoder.dart';
 import 'convert_latlang_geocoding.dart';
+import 'user_current_location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ConvertLatLangGeoCoding()
+      home: CurrectLocation()
     );
   }
 }
--- googlemap/lib/user_current_location.dart
+++ googlemap/lib/user_current_location.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CurrectLocation extends StatefulWidget {
@@ -20,7 +21,7 @@ class _CurrectLocationState extends State<CurrectLocation> {
       target: LatLng(23.7546655, 90.3914849), zoom: 19.151926040649414);
 
   List<Marker> _marker = [];
-  final List<Marker> _list = const [
+  final List<Marker> _list = [
     Marker(
         markerId: MarkerId('1'),
         position: LatLng(23.798393, 90.333131),
@@ -29,20 +30,54 @@ class _CurrectLocationState extends State<CurrectLocation> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        markers: Set<Marker>.of(_marker),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
+    return SafeArea(
+      child: Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        floatingActionButton: Align(
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton(
 
-        },
-        child: Icon(Icons.local_activity),
+            onPressed: () {
+              getUserCurrentLocaion().then((value) async {
+                print('My Current Locaion : ');
+                print(value.latitude.toString() + value.longitude.toString());
+
+                _marker.add(Marker(
+                    markerId: MarkerId("2"),
+                    position: LatLng(value.latitude, value.longitude),
+                    infoWindow: InfoWindow(title: 'My current Locaion')));
+                CameraPosition cameraPosition =
+                    CameraPosition(
+                        zoom: 14,
+                        target: LatLng(value.latitude, value.longitude));
+                final GoogleMapController controller = await _controller.future;
+                controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+                setState(() {
+
+                });
+              });
+            },
+            child: Icon(Icons.gps_fixed),
+
+          ),
+        ),
+
       ),
     );
+  }
+
+  Future<Position> getUserCurrentLocaion() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      print('error ' + error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
   }
 }
